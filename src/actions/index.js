18d7255631b4a6500a407cd3f3d189b35a07a306
Joe Escobedo <joe_escob1023@live.com>
2021-08-13 15:24:46 -0700

    no errors but i have some of my data in the wrong spots, applicaiton isnt working correctly

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const getSmurf = () => {
+export const fetchSmurfs = () => {
   return (dispatch) => {
     dispatch(fetchStart());
     axios
@@ -13,6 +13,18 @@ export const getSmurf = () => {
         dispatch(fetchFail(err));
       });
   };
+};
+
+export const setError = (error) => {
+  return (dispatch) => {
+    dispatch(setError(error));
+  };
+};
+
+export const ADD_SMURF = "ADD_SMURF";
+
+export const addSmurf = (smurf) => {
+  return { type: ADD_SMURF, payload: smurf };
 };
 
 export const FETCH_START = "FETCH_START";
@@ -28,6 +40,11 @@ export const fetchSuccess = (smurf) => {
 export const FETCH_FAIL = "FETCH_FAIL";
 export const fetchFail = (error) => {
   return { type: FETCH_FAIL, payload: error };
+};
+
+export const SET_ERROR = "SET_ERROR";
+export const errorMessage = (error) => {
+  return { type: SET_ERROR, payload: error };
 };
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
